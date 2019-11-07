0c437f7f873acf6105821d44049128b15d793de9
kashishjagtiani <kashishjagtiani@10.89.140.148>
2019-11-07 22:02:58 +0800

    Commit from Yankees w/ o/p "Yankees won fair and square"

---
+++ src/Yankees.java
@@ -0,0 +1,12 @@
+
+public class Yankees {
+	public static void main(String arg[]) {
+		Yankees lib = new Yankees();
+		if(lib.someLibraryMethod())
+			System.out.println("Yankees won fair and square");
+	}
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
+
