7eb89edd8e0881eac01596fdc06e2f91511560aa
Kashish Jagtiani <kashishjagtiani@Kashishs-MacBook-Pro.local>
2019-11-07 23:26:24 +0800

    Changed for red sox

--- src/Yankees.java
+++ src/Yankees.java
@@ -3,7 +3,7 @@ public class Yankees {
 	public static void main(String arg[]) {
 		Yankees lib = new Yankees();
 		if(lib.someLibraryMethod())
-			System.out.println("Yankees won fair and square");
+			System.out.println("Redsox won fair and square");
 	}
     public boolean someLibraryMethod() {
         return true;
